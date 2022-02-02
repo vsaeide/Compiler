0aa0e63709389e530f3bb4ff6a5e8d8981ec39d7
Sabrineh <Sabrinehmokhtari@yahoo.com>
2022-02-03 01:10:31 +0330

    Updated symbol.py

--- symbol.py
+++ symbol.py
@@ -8,6 +8,39 @@ class Symbol:
         self.line_number = line_number
         self.type = type
 
-    # def __str__(self):
-    #     output = '{}\t{}\t{}\t{}\t{}\t{}\n'.format(self.name, self.address, self.scope, self.length, self.starts_at, self.type)
-    #     return output
+
+class SymbolTable:
+    def __init__(self):
+        self.symbols = []
+
+    def add_symbol(self, name, address, scope, length, starts_at, type):
+        self.symbols.append(Symbol(name, address, scope, length, starts_at, type))
+
+    def find_symbol_by_name(self, name, scope):
+        for symbol in self.symbols:
+            if symbol.name == name and symbol.scope == scope:
+                return symbol
+        for symbol in self.symbols:
+            if symbol.name == name and symbol.scope is None:
+                return symbol
+        return 'first'
+
+    def find_symbol_by_address(self, address, scope):
+        for symbol in self.symbols:
+            if symbol.address == address and (symbol.scope == scope or symbol.scope is None):
+                return symbol
+
+    def get_func_params(self, function_name, number_of_parameter):
+        output = []
+        for i in range(len(self.symbols)):
+            if self.symbols[i].scope == function_name and len(output) < number_of_parameter:
+                output.append(self.symbols[i])
+        return output
+
+    def is_first_function(self, function_name):
+        first_function = ''
+        for i in range(len(self.symbols)):
+            if self.symbols[i].type.endswith('function'):
+                first_function = self.symbols[i].name
+                break
+        return first_function == function_name
